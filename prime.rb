1f7c316785177326eae080a4968c420869bb0165
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 20:33:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+
+def prime?(int)
+  test_nums = [3, 5, 7, 11]
+  infinite_test_num = (2...int)
+  if int == 2 || test_nums.include?(int)
+    true 
+  elsif int <= 1 
+    false
+  else 
+    if infinite_test_num.any? {|num| int % num == 0}
+      false 
+    else 
+      true 
+    
+    end 
+  end 
+end 
