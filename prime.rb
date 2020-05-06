447198857963b4d6f15d949e97bad7b1c44dbc17
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 20:40:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,11 +2,9 @@ require 'pry'
 
 
 def prime?(int)
-  test_nums = [3, 5, 7, 11]
+  
   infinite_test_num = (2...int)
-  if int == 2 || test_nums.include?(int)
-    true 
-  elsif int <= 1 
+  if int <= 1 
     false
   else 
     if infinite_test_num.any? {|num| int % num == 0}
